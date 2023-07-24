0b23e5ae87ea9118ae9140737445141068b6753f
unknown <goluchoudhary7808@gmail.com>
2023-07-24 14:24:06 +0530

    done

--- client/src/components/homepage/home.js
+++ client/src/components/homepage/home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./home.css"
-import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom' 
+import {BsFillPersonFill}  from "react-icons/bs"
+
+
 function Home(props){
     const history=useNavigate()
     console.log(props.email)
     return(
         <div className='home'>
            <h1>home</h1>
+           <BsFillPersonFill/>
            <h4>{props.email}</h4>
            <div className='button' onClick={()=>history("/login")} >Logout</div>
         </div>
---
+++ server/index.js
@@ -0,0 +1,83 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const cors=require("cors");
+const app=express();
+const dotenv =require("dotenv"); 
+dotenv.config();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors({origin:"*"}));
+mongoose.set('strictQuery', false);
+
+//mongoose connection
+mongoose.connect(process.env.db,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true 
+}).then(()=>{
+    console.log("mongodb connected");
+}).catch((e)=>{
+   console.log(e);
+});
+
+
+// Mongoose schema
+const userSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+});
+
+//Mongoose model
+const User= new mongoose.model("User",userSchema);
+
+//login
+app.post("/login",(req,res)=>{
+    const {email,password}=req.body
+    User.findOne({email:email},(err,user)=>{
+        if(user){
+                if(password===user.password){
+                    res.send({massage:"Login Successful",user:user})
+                    
+                }else{
+                    res.send({massage:"Password didn't match"})
+                   
+                }
+        }else{
+           
+            res.send({massage:"User not registered"})
+        }
+    })
+})
+
+
+//register
+app.post("/register",(req,res)=>{
+    const {name,email,password}=req.body.user
+    User.findOne({email:email},(err,user)=>{
+         if(user){
+            res.send({massage:"User already register"})
+             
+         }else{
+            const user=new User({
+                name,
+                email, 
+                password
+            })
+            user.save(err=>{
+                if(err){
+                    res.send(err)
+                }else{
+                    res.send({massage:"Successfully Registered, please login now."});
+                }
+            });
+         }
+    })
+  
+});
+
+//server connection
+app.listen(process.env.PORT,()=>{
+    console.log("server 8000"); 
+}); 
